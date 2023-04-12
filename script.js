460dc60b283212c393ef0ebd168af66e353635d4
Lucas Arruda <arluckis@gmail.com>
2023-04-12 14:32:18 -0300

    beta 4

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var bottomLigar = 1;
 const botoes = ['bottom1','bottom2','bottom3']
 for(var index = 0; index<botoes.length; index++){
-    var 'botoes[index]' = document.bottomLigar
-    console.log(botoes[index])
+    var botoes[index] = document.bottomLigar;
+    console.log(botoes[index]);
 }
